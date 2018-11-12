28f81cbb00c67f1cdb4d280106820f7d049ec03c
Phil Mucci <phil@minimalmetrics.com>
2018-11-12 15:13:30 +0100

    Added sample job import file and code to read it into a Pandas Dataframe

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from pony.orm import *
 import settings
 from models import db, Job, Host, Process, Thread, Metric
 import sys, time, datetime
-
+import pandas as pd
+import glob
 # 
 # Logical Model
 #
 @db_session
 def test_insert(jobid):
+		h = Host(name="foo.bar.com")
 		j = Job(jobid=jobid)
-		h = Host(name = "foo.bar.com")
 		p = Process(exename='a.out', path='/home/phil', host=h, pid=1011, ppid=1, sid=1, job=j)
 		t = Thread(tid=1011, duration=datetime.timedelta(minutes=1),process=p)
 		m1 = Metric(name='cycles', value=1001, threads=[t])
@@ -28,8 +29,21 @@ def test_query(jobid):
 		for t in p.threads:
 			for m in t.metrics:
 				print m.name, m.value
-		
 
+def load_jobcsv(jobid, hostname, pattern="job-[0-9]*.csv", dirname="./"):
+	print dirname+pattern
+	print glob.glob(dirname+pattern)
+	for f in glob.glob(dirname+pattern):
+		print f
+		pf = pd.read_csv(f)
+		print pf
+		thread_cnt = len(pf.index)
+		metric_cnt = len(pf.columns) - 8
+		metric_names = pf.columns[8:].values.tolist()
+		print thread_cnt, "threads with ", metric_cnt, "metrics", metric_names 
+#		print pf['tid']
+		for index, row in pf.iterrows():
+			print row['tid']
 
 try:
 	# db.bind(provider='postgres', user='postgres', password='example', host='0.0.0.0', dbname='EPMT')
@@ -41,11 +55,13 @@ except Exception as e:
 
 try:
 	test_insert(1)
+	test_query(1)
+	load_jobcsv(hostname="foo.bar.com", jobid=2)
 except Exception as exception:
 	print type(exception).__name__,exception.args
 	exit(1)
 
-test_query(1)
+
 
 #if __name__ == '__name__':
 #	app = App()
