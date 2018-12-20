8cbf66e05dda184d143b8d6deb1a3aebfc37e553
Phil Mucci <phil@minimalmetrics.com>
2018-12-20 15:02:12 +0100

    Updated settings

--- settings.py
+++ settings.py
@@ -1,9 +1,1 @@
-# settings.py
-db_params = {'provider': 'sqlite', 'filename': ':memory:'}
-#db_params_postgres = {'provider': 'postgres', 'user': 'postgres','password': 'example','host': 'db', 'dbname': 'EPMT'}
-#db.bind(provider='postgres', user='postgres', password='example', host='0.0.0.0', dbname='EPMT')
-# number of columns until caliper
-metrics_offset = 12
-debug = False
-input_pattern = "*-papiex-[0-9]*-[0-9]*.csv"
-install_prefix = "../papiex-oss/papiex-oss-install"
+settings_sqllite.py
