e9705e6b308528bfcc70514d5ea0fef3850a1ecb
Philip Mucci <phil@minimalmetrics.com>
2019-02-06 16:53:42 +0100

    Added trailing slash

--- settings_sqllite.py
+++ settings_sqllite.py
@@ -1,6 +1,7 @@
 # settings.py
 db_params = {'provider': 'sqlite', 'filename': ':memory:'}
 metrics_offset = 12
+PAPIEX_OPTIONS = "PERF_COUNT_SW_CPU_CLOCK"
 debug = False
 input_pattern = "*-papiex-[0-9]*-[0-9]*.csv"
-install_prefix = "../papiex-oss/papiex-oss-install"
+install_prefix = "../papiex-oss/papiex-oss-install/"
