7e892324dff3a91b08380a7594905b947fac7ba6
Tushar Mohan <tusharmohan@gmail.com>
2019-06-04 11:47:10 +0530

    - we now merge the threads_sums fields into the process columns by default (merge_sums_field = True).
    This flattens the structure and the user doesn't need to deal with a nested dictionary.
    
    - also added support to order the results

--- query_api.py
+++ query_api.py
@@ -5,8 +5,7 @@ print(settings.db_params)
 setup_orm_db(settings)
 
 
-PROC_EXCLUDE_FIELDS = ['threads_df']
-
+THR_SUMS_FIELD = 'threads_sums'
 # Filter a supplied set of Process objects to find a match
 # by tags or some primary keys. If the process_set is not
 # specified then the query will be run against all processes.
@@ -15,16 +14,25 @@ PROC_EXCLUDE_FIELDS = ['threads_df']
 #        lambda p: p.duration > 1000
 #        OR
 #        'p.duration > 1000 and p.numtids < 4'
-# 'limit' if set, limits the total number of results
+# limit: if set, limits the total number of results
 # 
-# 'out_fmt': Output format, is one of 'dict', 'orm', 'pandas'
-#            The default is 'dict', which case each process is
-#            output as a python dictionary, and the output is 
-#            a list of dictionaries.
-#            'pandas': output is a pandas dataframe
-#            'orm': output is a list of ORM objects
+# fmt :   Output format, is one of 'dict', 'orm', 'pandas'
+#         'dict': This is the default, and in this case
+#                 each process is output as a python dictionary, 
+#                 and the entire output is a list of dictionaries.
+#         'pandas': output is a pandas dataframe
+#         'orm': output is a list of ORM objects
 #
-# example, to get all processes for a particular Job, with jobid '32046', which
+# merge_sum_fields: By default, this is True, and this means threads sums are
+#          are folded into the process. If set to False, the threads'
+#          sums will be available as a separate field THR_SUMS_FIELD.
+#          Flattening makes subsequent processing easier as all the
+#          thread aggregates such as 'usertime', 'systemtime' are available
+#          as first-class members of the process. This option is silently
+#          ignored if output format 'fmt' is set to 'orm', and ORM
+#          objects will not be merge_sum_fieldsed.
+#
+# For example, to get all processes for a particular Job, with jobid '32046', which
 # are multithreaded, you would do:
 #
 #   filter_processes(jobs = ['32046'], fltr = 'p.numtids > 1')
@@ -33,9 +41,21 @@ PROC_EXCLUDE_FIELDS = ['threads_df']
 # filter_processes(tags = {'app': 'fft'})
 #
 # to get a pandas dataframe:
-# qs1 = filter_processes(tags = {'app': 'fft'}, out_fmt = 'pandas')
+# qs1 = filter_processes(tags = {'app': 'fft'}, fmt = 'pandas')
 #
-def filter_processes(jobs = [], tags = {}, fltr = None, limit = 0, out_fmt='dict'):
+# to filter processes for a job '1234' and order by process duration,
+# getting the top 10 results, and keeping the final output in ORM format:
+# 
+# q = filter_processes(['1234'], order = 'desc(p.duration)', limit=10, fmt='orm')
+#
+# now, let's filter processes with duration > 100000 and order them by user+system time,
+# and let's get the output into a pandas dataframe:
+# q = filter_processes(fltr = (lambda p: p.duration > 100000), order = 'desc(p.threads_sums["user+system"]', fmt='pandas')
+# Observe, that while 'user+system' is a metric available in the threads_sums field,
+# by using the default merge_sum_fields=True, it will be available as column in the output
+# dataframe. The output will be pre-sorted on this field because we have set 'order'
+#
+def filter_processes(jobs = [], tags = {}, fltr = None, order = '', limit = 0, fmt='dict', merge_sum_fields=True):
     if jobs:
         # is jobs a collection of Job IDs or actual Job objects?
         if type(jobs[0]) == str or type(jobs[0]) == unicode:
@@ -56,17 +76,29 @@ def filter_processes(jobs = [], tags = {}, fltr = None, limit = 0, out_fmt='dict
     if fltr:
         qs = qs.filter(fltr)
 
+    if order:
+        qs = qs.order_by(order)
+
     # finally set limits on the number of processes returned
     if limit:
         qs = qs.limit(int(limit))
 
-    # return a list of processes, but first make sure we
-    # convert each of the Process objects to a dictionary (excluding blacklisted
-    # fields) unless conv_orm_objects is False
+    if fmt == 'orm':
+        return qs[:]
+
+    # convert the ORM into a list of dictionaries, excluding blacklisted fields
     proc_exclude_fields = settings.query_process_fields_exclude if hasattr(settings, 'query_process_fields_exclude') else []
-    if out_fmt == 'orm': return qs[:]
     out_list = [ p.to_dict(exclude = proc_exclude_fields) for p in qs ]
-    if out_fmt == 'pandas':
+
+    # do we need to merge threads' sum fields into the process?
+    if merge_sum_fields:
+        for p in out_list:
+            p.update(p[THR_SUMS_FIELD])
+        del p[THR_SUMS_FIELD]
+
+    if fmt == 'pandas':
         from pandas import DataFrame
         return DataFrame(out_list)
-    return out_list # just return a list of python dictionaries
+
+    # we assume the user wants the output in the form of a list of dicts
+    return out_list
