f05e4a7ac1ce495b18d515830fef87077758d383
Tushar Mohan <tushar@minimalmetrics.com>
2019-06-06 11:05:04 +0530

    fixed typo in script

--- sample/kernel/build-linux-kernel.sh
+++ sample/kernel/build-linux-kernel.sh
@@ -5,8 +5,7 @@
 
 build_dir=$(tempfile -p epmt -s build)
 echo "creating build directory: $build_dir"
-mkdir $build_dir
-cd $build_dir
+rm -rf $build_dir; mkdir -p $build_dir && cd $build_dir
 
 # download
 PAPIEX_ARGS="operation:download,operation_count:1;instance:1" wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.7.tar.xz
@@ -14,7 +13,8 @@ PAPIEX_ARGS="operation:extract,operation_count:2;instance:1" tar -xf linux-5.1.7
 cd linux-5.1.7
 
 # configure
-(export PAPIEX_ARGS="operation:configure,operation_count:3;instance:1" ; cp -v /boot/config-$(uname -r) .config; ./configure; make olddefconfig)
+cp -v /boot/config-$(uname -r) .config
+PAPIEX_ARGS="operation:configure,operation_count:3;instance:1" make olddefconfig
 
 
 # build
