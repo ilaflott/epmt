c74891743250396c0e23c850d267eadca960ab81
Tushar Mohan <tushar@minimalmetrics.com>
2019-06-06 11:37:43 +0530

    working synthetic script that we are running to generate new data using a linux kernel compile with separate download, configure and build phases.

--- sample/kernel/build-linux-kernel.sh
+++ sample/kernel/build-linux-kernel.sh
@@ -3,6 +3,9 @@
 # you will need the following deps installed:
 #  sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev coreutils
 
+# EPMT_JOB_TAGS='model:linux-kernel;compiler:gcc' ./epmt -a -j kernel-build-$(date +%Y%m%d-%H%M%S) run sample/kernel/build-linux-kernel.sh
+#
+
 build_dir=$(tempfile -p epmt -s build)
 echo "creating build directory: $build_dir"
 rm -rf $build_dir; mkdir -p $build_dir && cd $build_dir
@@ -15,7 +18,6 @@ cd linux-5.1.7
 # configure
 cp -v /boot/config-$(uname -r) .config
 PAPIEX_ARGS="operation:configure,operation_count:3;instance:1" make olddefconfig
-
 
 # build
 PAPIEX_ARGS="operation:build,operation_count:4;instance:1" make -j $(nproc)
