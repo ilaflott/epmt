2f6121415994c771667b0de528fd553efd404ce8
Tushar Mohan <tushar@minimalmetrics.com>
2019-06-06 13:13:50 +0530

    fixed so many typos :-(

--- sample/kernel/build-linux-kernel.sh
+++ sample/kernel/build-linux-kernel.sh
@@ -11,14 +11,14 @@ echo "creating build directory: $build_dir"
 rm -rf $build_dir; mkdir -p $build_dir && cd $build_dir
 
 # download
-PAPIEX_ARGS="operation:download,operation_count:1;instance:1" wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.7.tar.xz
-PAPIEX_ARGS="operation:extract,operation_count:2;instance:1" tar -xf linux-5.1.7.tar.xz
+PAPIEX_TAGS="operation:download;operation_count:1;instance:1" wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.7.tar.xz
+PAPIEX_TAGS="operation:extract;operation_count:2;instance:1" tar -xf linux-5.1.7.tar.xz
 cd linux-5.1.7
 
 # configure
 cp -v /boot/config-$(uname -r) .config
-PAPIEX_ARGS="operation:configure,operation_count:3;instance:1" make olddefconfig
+PAPIEX_TAGS="operation:configure;operation_count:3;instance:1" make olddefconfig
 
 # build
-PAPIEX_ARGS="operation:build,operation_count:4;instance:1" make -j $(nproc)
+PAPIEX_TAGS="operation:build;operation_count:4;instance:1" make -j $(nproc)
 
