fb4a6bcd2a109c415da959306aa48a272176d739
Tushar Mohan <tushar@minimalmetrics.com>
2019-06-06 10:50:09 +0530

    added script to download and build the kernel with suitable tags

---
+++ sample/kernel/build-linux-kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+# you will need the following deps installed:
+#  sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev coreutils
+
+build_dir=$(tempfile -p epmt -s build)
+echo "creating build directory: $build_dir"
+mkdir $build_dir
+cd $build_dir
+
+# download
+PAPIEX_ARGS="operation:download,operation_count:1;instance:1" wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.1.7.tar.xz
+PAPIEX_ARGS="operation:extract,operation_count:2;instance:1" tar -xf linux-5.1.7.tar.xz
+cd linux-5.1.7
+
+# configure
+(export PAPIEX_ARGS="operation:configure,operation_count:3;instance:1" ; cp -v /boot/config-$(uname -r) .config; ./configure; make olddefconfig)
+
+
+# build
+PAPIEX_ARGS="operation:build,operation_count:4;instance:1" make -j $(nproc)
+
