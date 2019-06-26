393a2a20a40006926dfb6e2f963b69ffec3124f4
Philip Mucci <phil@minimalmetrics.com>
2019-06-26 11:16:28 +0200

    Better error checking and handling

--- epmt-example-stage-submit.sh
+++ epmt-example-stage-submit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-rm -rf /tmp/epmt/615503
-mkdir /tmp/epmt/615503
-tar -C /tmp/epmt/615503 -xzf sample/uncollated/postprocruns/ESM4_historical_D151_atmos_18540101.papiex.615503.tgz
-epmt -v -j 615503 stage
-epmt -v submit ./615503.tgz
+set -Eeuxo pipefail
+USER=testuser
+JOB=615503
+rm -rf /tmp/epmt
+mkdir -p /tmp/epmt/$USER/epmt/$JOB
+tar -C /tmp/epmt/$USER/epmt/$JOB -xzf sample/uncollated/postprocruns/ESM4_historical_D151_atmos_18540101.papiex.$JOB.tgz
+epmt -v -j $JOB stage
+epmt -v submit ./$JOB.tgz
