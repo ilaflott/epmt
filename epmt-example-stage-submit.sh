1fbebd627aaf70e18895ccd0c43d3adaedad9fa7
Philip Mucci <phil@minimalmetrics.com>
2019-06-26 12:19:16 +0200

    Renamed

--- epmt-example-stage-submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -Eeuxo pipefail
-export USER=testuser
-JOB=615503
-rm -rf /tmp/epmt
-mkdir -p /tmp/epmt/$USER/epmt/$JOB
-tar -C /tmp/epmt/$USER/epmt/$JOB -xzf sample/uncollated/postprocruns/ESM4_historical_D151_atmos_18540101.papiex.$JOB.tgz
-epmt -v -j $JOB stage
-epmt -v submit ./$JOB.tgz
