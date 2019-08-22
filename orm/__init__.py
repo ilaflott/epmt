24ab18307214550dd194ede39ac16ba1c7f76c94
Tushar Mohan <tushar@minimalmetrics.com>
2019-08-22 12:00:31 +0530

    cosmetic change

--- orm/__init__.py
+++ orm/__init__.py
@@ -1,12 +1,6 @@
-# /models/__init__.py
-#from pony import orm
-#from .general import db
 import settings
 
 if settings.orm == 'sqlalchemy':
     from .sqlalchemy import *
 else:
     from .pony import *
-
-#from .logical_model import *
-#from .measurement_model import *
