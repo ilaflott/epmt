a1d85a8e8d6ff3bd5ad20b3d999735b27e269b08
Chris Ault <sp33dsk8@gmail.com>
2019-09-16 12:50:55 -0500

    Pony Warning message dbsize not yet implemented for pony

--- orm/pony/dbapi.py
+++ orm/pony/dbapi.py
@@ -7,9 +7,15 @@
 # to DB-specific information. All the functions below
 # rely on the ORM being Pony
 
-def get_db_size():
+def get_db_size(findwhat, other):
+    import settings
+    from logging import getLogger
+    logger = getLogger(__name__)  # you can use other name
+    logger.warning("Pony not yet implemented for db sizing, use sqlalchemy in settings.py")
+    return 0
     #from general import xyz
     # If db is 'pg'
     # elif db is sqlite3
+
     pass
 
