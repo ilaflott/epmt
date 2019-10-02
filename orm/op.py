a44e4e7166e8e62ab3e3f0966eac1690fb5ccb12
Tushar Mohan <tushar@minimalmetrics.com>
2019-10-02 13:58:34 +0530

    - speed up in Operation by using lazy-evaluation - indexed tags in various models (this will help speedup search   on tags under postgres) - renamed Operation.tag -> Operation.tags since, now, it can also   take a list of tags to define a 'higher-order' operation.

--- orm/op.py
+++ orm/op.py
@@ -13,25 +13,18 @@ class Operation(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-    def __init__(self, jobs, tag, exact_tag_only = False):
-        from epmt_query import get_procs
+    def __init__(self, jobs, tags, exact_tag_only = False):
         from orm import orm_is_query
         from epmtlib import tag_from_string, tags_list
+        from logging import getLogger
+        logger = getLogger(__name__)
         if (orm_is_query(jobs) and jobs.count() == 0) or (len(jobs) == 0):
             raise ValueError("jobs count should be greater than zero")
         self.jobs = jobs
-        self.tag = tags_list(tag) if (type(tag) == list) else tag_from_string(tag)
-        self.processes = get_procs(jobs = jobs, tags = tag, exact_tag_only = exact_tag_only, fmt='orm')
-        if len(self.processes[:]) == 0:
-            from logging import getLogger
-            logger = getLogger(__name__)
-            logger.warning("No processes found for operation -- {0}".format(tag))
-            return None
-        else:
-            self.start = min(p.start for p in self.processes)
-            self.end = max(p.end for p in self.processes)
-
+        self.tags = tags_list(tags) if (type(tags) == list) else tag_from_string(tags)
+        self.exact_tag_only = exact_tag_only
         # this will be initialized on first reference
+        self._processes = None
         self._proc_sums = None
         self._intervals = None
         self._duration = None
@@ -43,13 +36,34 @@ class Operation(dict):
         return len(self.intervals)
 
     @property
+    def processes(self):
+        if self._processes is None:
+            from logging import getLogger
+            from epmt_query import get_procs
+            logger = getLogger(__name__)
+            logger.debug('computing op processes..')
+            self._processes = get_procs(jobs = self.jobs, tags = self.tags, exact_tag_only = self.exact_tag_only, fmt='orm')
+            if len(self._processes[:]) == 0:
+                logger.warning("No processes found for operation -- {0}".format(tags))
+            # else:
+            #     logger.debug('computing op start/end times..')
+            #     self.start = min(p.start for p in self.processes)
+            #     self.end = max(p.end for p in self.processes)
+        return self._processes
+
+
+    @property
     def intervals(self):
         '''
         Returns a list of non-overlapping intervals for an operation
         '''
         from epmtlib import merge_intervals
-        if self._intervals is None: 
+        if self._intervals is None:
+            from logging import getLogger
+            logger = getLogger(__name__)
+            logger.debug('computing operation intervals..') 
             _intervals = [[p.start, p.end] for p in self.processes]
+            logger.debug('merging operation intervals..') 
             _mi = merge_intervals(_intervals)
             self._intervals = tuple([tuple(i) for i in _mi])
         return self._intervals
@@ -66,9 +80,12 @@ class Operation(dict):
         if self._proc_sums is None: 
             from epmt_query import get_op_metrics
             from epmtlib import sum_dicts_list
-            op_metrics = get_op_metrics(jobs = self.jobs, tags = self.tag, exact_tags_only = False, group_by_tag=True, fmt='dict')
-            if type(self.tag) == list:
-                assert(len(op_metrics) == len(self.tag))
+            from logging import getLogger
+            logger = getLogger(__name__)
+            logger.debug('getting op_metrics for jobs={0}, tags={1}'.format(self.jobs, self.tags))
+            op_metrics = get_op_metrics(jobs = self.jobs, tags = self.tags, exact_tags_only = False, group_by_tag=True, fmt='dict')
+            if type(self.tags) == list:
+                assert(len(op_metrics) == len(self.tags))
             else:
                 assert(len(op_metrics) == 1)
             self._proc_sums = sum_dicts_list(op_metrics, exclude = ['tags'])
@@ -83,5 +100,5 @@ class Operation(dict):
         d['processes'] = self.processes[:]
         d['contiguous'] = (len(intervals) == 1)
         d['num_runs'] = len(intervals) 
-        for attr in ('_duration', '_intervals', '_proc_sums'): del d[attr]
+        for attr in ('_duration', '_intervals', '_proc_sums', '_processes'): del d[attr]
         return d
