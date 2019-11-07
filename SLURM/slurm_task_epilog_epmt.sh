abe968bbeee4e15605e877c56b7b83dd8d797583
Phil Mucci <phil@minimalmetrics.com>
2019-11-07 18:24:03 +0100

    Updates for new release tests

--- SLURM/slurm_task_epilog_epmt.sh
+++ SLURM/slurm_task_epilog_epmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unset LD_PRELOAD
-EPMT_PATH=/opt/epmt/epmt
+EPMT_PATH=/opt/epmt/epmt-install/epmt
 if [ -x $EPMT_PATH ]; then
     if [ ! -z "$SLURM_LOCALID" ] && [ $SLURM_LOCALID == "0" ]; then
 	$EPMT_PATH stop
--- SLURM/slurm_task_prolog_epmt.sh
+++ SLURM/slurm_task_prolog_epmt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EPMT_PATH=/opt/epmt/epmt
+EPMT_PATH=/opt/epmt/epmt-install/epmt
 if [ -x $EPMT_PATH ]; then
     if [ ! -z "$SLURM_LOCALID" ] && [ $SLURM_LOCALID == "0" ]; then
 	$EPMT_PATH start
