3ce8674d99587b48d765edf7e744deb2e977a70f
Phil Mucci <phil@minimalmetrics.com>
2019-11-20 13:13:08 +0100

    Add default CWD serving dir

--- epmt_cmd_notebook.py
+++ epmt_cmd_notebook.py
@@ -20,20 +20,22 @@ def epmt_notebook(arglist):
     if mode == "kernel":  # run iPython kernel with passed ops
         args = ["kernel"]
         args.extend(cmd_args)
-        from IPython import start_ipython, start_kernel
         # This does not want argv[0]
         logger.info("ipython kernel argv: %s",str(args))
+        from IPython import start_ipython, start_kernel
         rv = start_ipython(argv=args)
     else:                 # Run IPython Notebook with passed ops
         import sys
-        import os.path
-        me = os.path.realpath(sys.argv[0])
+        from os.path import realpath
+        from os import getcwd
+        me = realpath(sys.argv[0])
         logger.debug("Using %s as binary" ,me)
         args = [me]
+        args.extend(["--notebook-dir="+getcwd(),
+                     "--KernelManager.kernel_cmd=['"+me+"', 'notebook', 'kernel', '--', '-f', '{connection_file}']"])
         args.extend(all_args)
-        args.extend(["--KernelManager.kernel_cmd=['"+me+"', 'notebook', 'kernel', '--', '-f', '{connection_file}']"])
-        from notebook import notebookapp
         logger.info("notebook argv: %s",str(args))
+        from notebook import notebookapp
         rv = notebookapp.launch_new_instance(argv=args)
     return True
 
