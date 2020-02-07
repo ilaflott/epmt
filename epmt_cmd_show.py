a0f06c8101fd4b50affa27c5e8c9ff8561457a4a
Tushar Mohan <tushar@minimalmetrics.com>
2020-02-07 09:46:11 +0530

    - use standard eq.get_jobs in preference to Job orm constructs - handle error condition when job is not found in db (for epmt show)

--- epmt_cmd_show.py
+++ epmt_cmd_show.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
+import epmt_query as eq
+from logging import getLogger
 
 def epmt_show_job(jobid):
-    from orm import Job, orm_to_dict
-    import epmt_query # to set up db and init stuff
+    logger = getLogger(__name__)  # you can use other name
     if type(jobid) == list:
         jobid = jobid[0]
-    j = Job[jobid]
-    # EXCLUDE_ATTR = { 'processes', 'metadata' }
-    j_dict = orm_to_dict(j)
+    jobs = eq.get_jobs([jobid], fmt='dict')
+    if len(jobs) != 1:
+        logger.error('Job %s could not be found in database' % jobid)
+        return False
+    j_dict = jobs[0]
     for key in sorted(j_dict.keys()):
         # if attr.startswith('_'): continue
         # if attr in EXCLUDE_ATTR: continue
