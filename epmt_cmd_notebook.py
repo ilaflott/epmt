86d831e3f18517bd83d5e02e27e44882d0339a89
Phil Mucci <phil@minimalmetrics.com>
2021-02-23 19:26:39 +0100

    Fixes for path to python kernel when frozen

--- epmt_cmd_notebook.py
+++ epmt_cmd_notebook.py
@@ -6,6 +6,23 @@ logger = getLogger(__name__)  # you can use other name
 
 def epmt_notebook(arglist):
     logger.info("epmt_notebook: %s",str(arglist))
+    import sys
+    from os import getcwd, path
+
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):  # we are running in a pyinstaller bundle
+        frozen = 'ever so'
+        bundle_dir = getattr(sys, '_MEIPASS', '')
+        me = "'"+sys.executable + "'"
+    else: # we are running in a normal Python environment
+        frozen = 'not'
+        bundle_dir = path.dirname(path.abspath(__file__))
+        me = "'"+sys.executable + "','" + sys.argv[0]+"'"    
+    logger.debug( 'we are %s',frozen+' frozen')
+    logger.debug( 'sys.argv[0] is %s', sys.argv[0] )
+    logger.debug( 'sys.executable is %s', sys.executable )
+    logger.debug( 'os.getcwd is %s', getcwd() )
+    logger.debug( 'bundle/module dir is %s', bundle_dir )
+    logger.debug( 'epmt is %s',me)
 
     mode = None
     cmd_args = []
@@ -18,23 +35,29 @@ def epmt_notebook(arglist):
             cmd_args.append(arg)
 
     if mode == "kernel":  # run iPython kernel with passed ops
+        from IPython import start_ipython
         args = ["kernel"]
         args.extend(cmd_args)
         # This does not want argv[0]
-        logger.info("ipython kernel argv: %s",str(args))
-        from IPython import start_ipython
+        logger.info("mode kernel argv: %s",str(args))
         start_ipython(argv=args)
     else:                 # Run IPython Notebook with passed ops
-        import sys
-        from os.path import realpath
-        from os import getcwd
-        me = realpath(sys.argv[0])
-        logger.debug("Using %s as binary" ,me)
+        from logging import root
+        debug=""
+        if (root.level < 30):
+            debug="-v"
+        if (root.level < 20):
+            debug+="v"
+        if (root.level < 10):
+            debug+="v"
+        if debug:
+            debug = ", '"+debug+"'"
+        logger.debug('debug is %s',debug)
         args = []
         args.extend(["--notebook-dir="+getcwd(),
-                     "--KernelManager.kernel_cmd=['"+me+"', 'notebook', 'kernel', '--', '-f', '{connection_file}']"])
+                     "--KernelManager.kernel_cmd=["+me+debug+", 'notebook', 'kernel', '--', '-f', '{connection_file}']"])
         args.extend(all_args)
-        logger.info("notebook argv: %s",str(args))
+        logger.info("mode notebook argv: %s",str(args))
         from notebook import notebookapp
         notebookapp.launch_new_instance(argv=args)
     return True
