7754ab5e05957fb6a13cd194d8df5ddbe26b399d
Ian <ilaflott@gmail.com>
2023-08-31 13:43:43 -0400

    ubuntu:latest --> phusion/baseimage:0.10.2 for phusion dockerfiles

--- clone_and_build.sh
+++ clone_and_build.sh
@@ -10,27 +10,38 @@ papiexREV=ubuntu-build
 repo=ilaflott
 builddir=current_ubuntu_build_epmt
 
-if [ ! -d $builddir ]
-then
-    mkdir $builddir
-else
-    echo "error! builddir exists"
-    return
-fi
 
-cd $builddir
+[ ! -d $builddir ] || echo "error! builddir exists"
+[ -d $builddir ] && return 
+mkdir $builddir && cd $builddir
+
+#if [ ! -d $builddir ]
+#then
+#    mkdir $builddir
+#else
+#    echo "error! builddir exists"
+#    return
+#fi
+
+
 
 ## Clone up the repos
 git clone -b feature/$papiexREV git@gitlab.com:$repo/papiex.git papiex-oss
-#sed -i -e '$a[user]' ./papiex/.git/config
-#sed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' ./papiex/.git/config
+gsed -i -e '$a[user]' ./papiex-oss/.git/config
+gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' ./papiex-oss/.git/config
 
 
-git clone --recursive -b feature/$epmtREV git@gitlab.com:$repo/epmt.git epmt.git
-#sed -i -e '$a[user]' epmt.git/.git/config
-#sed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' epmt.git/.git/config
+#git clone --recursive -b feature/$epmtREV git@gitlab.com:$repo/epmt.git epmt.git
+git clone -b feature/$epmtREV git@gitlab.com:$repo/epmt.git epmt.git
+gsed -i -e '$a[user]' epmt.git/.git/config
+gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' epmt.git/.git/config
 
+cp ./clone_and_build.sh ./epmt.git
 cd epmt.git
+git submodule update --init --recursive --remote
+
+#comment this return out to clone + build without stopping. 
+return
 
 ## note- messes with my terminal prompt (PS1)
 export PS1="[$(__shortpath "\W") \$] "
