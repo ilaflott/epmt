9e466fb5fbceed9317855a7446643adfb906df54
Ian <ilaflott@gmail.com>
2023-09-12 13:05:29 -0400

    tweak clone/build script

--- clone_and_build.sh
+++ clone_and_build.sh
@@ -1,5 +1,41 @@
 #!/bin/bash -e
 
+#epmtREV=7.pip-install
+#papiexREV=mkdir-perm-issue
+#repo=minimal-metrics-llc/epmt
+#builddir=current_centos_build_epmt
+
+#epmtREV=feature/7.pip-install-ubuntu
+#epmtREV=feature/7.pip-install-ubuntu2
+epmtREV=feature/7.pip-install-ubuntu2.diff.build.reqs
+papiexREV=feature/ubuntu-build
+repo=ilaflott
+builddir=diff_ubuntu_build_epmt
+    
+
+
+function clone_epmt(){
+    
+    [ ! -d $builddir ] || echo "error! builddir exists"
+    [ -d $builddir ] && return 
+    mkdir $builddir && cd $builddir
+    
+     ## Clone up the repos
+    git clone -b $papiexREV --single-branch git@gitlab.com:$repo/papiex.git papiex-oss
+    gsed -i -e '$a[user]' ./papiex-oss/.git/config
+    gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' ./papiex-oss/.git/config
+    
+    git clone -b $epmtREV --single-branch git@gitlab.com:$repo/epmt.git epmt.git
+    gsed -i -e '$a[user]' epmt.git/.git/config
+    gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' epmt.git/.git/config
+
+    cd epmt.git && git submodule update --init --recursive --remote
+    cp ../../clone_and_build.sh .
+    
+    return
+}
+
+
 function build_epmt(){
     ## note- messes with my terminal prompt (PS1)
     export OLD_PS1=$PS1
@@ -7,40 +43,7 @@ function build_epmt(){
     if [ -f FRESH.log ]; then rm -f FRESH.log; fi 
     exec > >(tee -a "FRESH.log") 2>&1
     make release
-    #make check-release
-    #export PS1=$OLD_PS1
+    return
 }    
 
 
-function clone_epmt(){
-    #epmtREV=7.pip-install
-    #papiexREV=mkdir-perm-issue
-    #repo=minimal-metrics-llc/epmt/
-    #builddir=current_centos_build_epmt
-    
-    epmtREV=7.pip-install-ubuntu2
-    papiexREV=ubuntu-build
-    repo=ilaflott
-    builddir=current_ubuntu_build_epmt
-    
-    
-    [ ! -d $builddir ] || echo "error! builddir exists"
-    [ -d $builddir ] && return 
-    mkdir $builddir && cd $builddir
-    
-     ## Clone up the repos
-    git clone -b feature/$papiexREV git@gitlab.com:$repo/papiex.git papiex-oss
-    gsed -i -e '$a[user]' ./papiex-oss/.git/config
-    gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' ./papiex-oss/.git/config
-    
-    
-    #git clone --recursive -b feature/$epmtREV git@gitlab.com:$repo/epmt.git epmt.git
-    git clone -b feature/$epmtREV git@gitlab.com:$repo/epmt.git epmt.git
-    gsed -i -e '$a[user]' epmt.git/.git/config
-    gsed -i -e '$a\ \ \ \ email\ =\ ilaflott@gmail.com' epmt.git/.git/config
-    cp ./clone_and_build.sh ./epmt.git && cd epmt.git
-    
-    git submodule update --init --recursive --remote
-    
-     return
- }
