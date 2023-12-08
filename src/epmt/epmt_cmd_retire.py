91ed9f2e4a88209914b0ccf35b7bb96217a2e4ac
Ian Laflotte <ian.laflotte@noaa.gov>
2023-12-08 16:40:23 -0500

    Revert "Revert "Merge branch 'feature/7.pip-install' into 'feature/7.pip-install'""
    This reverts commit d10fce5d23ea303bcb721642ab78ae39aac667e0.
    
    Apologies to the git history. Awhile back I was perusing my old MR for the retire
    bugfix, wanting to look up something in the notes there, and I managed to merge the changes
    accidentally. The MR in question is here:
    ttps://gitlab.com/minimal-metrics-llc/epmt/epmt/-/merge_requests/13
    
    This wasn't really a big deal, the code was essentially done, I thought I just
    shouldn't had done so in the moment, and panic-reverted the merge... that was the real
    mistake. As I've learned, reverting merge commits, and then re-doing them, tends to
    create a complicated/hard to parse dev history. See:
    https://mirrors.edge.kernel.org/pub/software/scm/git/docs/howto/revert-a-faulty-merge.txt
    
    Apparently the best thing to do is to revert the revert commit to re-do
    the merge. So that's what is happening now! All I've done here is a
    'git revert <commit-hash>', nothing else. For the changes this introduces,
    see the MR.

--- src/epmt/epmt_cmd_retire.py
+++ src/epmt/epmt_cmd_retire.py
@@ -1,18 +1,36 @@
 from __future__ import print_function
 
-def epmt_retire():
+def epmt_retire(skip_unprocessed = False, dry_run = False):
+
     import epmt.epmt_settings as settings
     from epmt.epmt_query import retire_jobs, retire_refmodels
     from logging import getLogger
-    logger = getLogger(__name__)  # you can use other name
-    num_jobs_retired = 0
+    logger = getLogger(__name__)
+    
     num_models_retired = 0
-
     logger.info('Retiring models older than %d days', settings.retire_models_ndays)
-    num_models_retired = retire_refmodels(settings.retire_models_ndays)
+    num_models_retired = retire_refmodels(settings.retire_models_ndays, dry_run=dry_run)
 
+    import tracemalloc as tm
+    tm.start()
+
+    #__________________
+    model_retire_size, model_retire_peak=tm.get_traced_memory()
+    print(f'after model retire: memory_size={model_retire_size/1024/1000} MiB, memory_peak={model_retire_peak/1024/1000} MiB')
+    tm.reset_peak()
+    #------------------
+
+    num_jobs_retired = 0
     logger.info('Retiring jobs older than %d days', settings.retire_jobs_ndays)
-    num_jobs_retired = retire_jobs(settings.retire_jobs_ndays)
+    num_jobs_retired = retire_jobs(settings.retire_jobs_ndays, skip_unprocessed = skip_unprocessed, dry_run=dry_run)
+
+    #__________________
+    job_retire_size, job_retire_peak=tm.get_traced_memory()
+    print(f'  after job retire: memory_size={job_retire_size/1024/1000} MiB, memory_peak={job_retire_peak/1024/1000} MiB')
+    tm.reset_peak()    
+    #------------------
 
     logger.info('%d jobs retired, %d models retired',num_jobs_retired, num_models_retired)
+    if dry_run:
+        print(f'(dry_run=True) {num_jobs_retired} jobs and {num_models_retired} models will be retired')
     return (num_jobs_retired, num_models_retired)
