7da3a99732c04c00766b15a924243cc741e4e0d4
Ian Laflotte <ilaflott@gmail.com>
2024-09-06 13:06:18 -0400

    print out sys.path entries to logging, also attempt import of epmt.settings instead

--- src/epmt/epmt_settings.py
+++ src/epmt/epmt_settings.py
@@ -2,16 +2,23 @@
 from epmt.epmt_default_settings import *
 from logging import getLogger, basicConfig, ERROR
 from sys import exit
+import sys.path
 
 # now load the user-specific settings.py so they override the defaults
+basicConfig(level=ERROR)
+logger = getLogger(__name__)
+logger.info("attempting import of user settings")
+logger.info("sys.path entries are:")
+for path in sys.path:
+    logger.info(f"path={path}")
+
 try:
     from settings import *
 except Exception as e:
-    basicConfig(level=ERROR)
-    logger = getLogger(__name__)
     if e.__class__ == ModuleNotFoundError:
-        logger.error(str(e)+": attempting relative import instead.")
-        from .settings import *
+        logger.error(str(e)+": attempting epmt.settings import instead.")
+        #from .settings import *
+        from epmt.settings import *
     else:
         raise Exception('an exception other than ModuleNotFoundError?')
 finally:
