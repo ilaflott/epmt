4a36f7429a6d7e238d73d67b7e4bd1971eedb7aa
Ian Laflotte <ilaflott@gmail.com>
2025-03-19 12:24:56 -0400

    adjust test_shell to actually run the only function shown in there- being missed because it doesnt start with test_. this seems to fail currently

--- src/epmt/test/test_shell.py
+++ src/epmt/test/test_shell.py
@@ -4,13 +4,17 @@
 from . import *
 
 class SHELLCmds(unittest.TestCase):
-    def run_cond(self):
+#    def run_cond(self):
+    def test_run_cond(self):
         with capture() as (out,err):
             #import argparse, json
-            #argns = argparse.Namespace(auto=True, drop=False, dry_run=False, epmt_cmd='dbsize', epmt_cmd_args=['run'], error=False, help=False, jobid=None, json=True, verbose=0)
+            #argns = argparse.Namespace(auto=True, drop=False, dry_run=False,
+            #                           epmt_cmd='dbsize', epmt_cmd_args=['run'],
+            #                           error=False, help=False, jobid=None, json=True, verbose=0 )
             from epmt.epmt_cmds import epmt_run
             # pylint: disable=redundant-keyword-arg
-            out = epmt_run("1", 'testuser', ["/bin/sleep","1"], wrapit=True, dry_run=False, debug=False)
+            out = epmt_run( "1", 'testuser', ["/bin/sleep","1"],
+                            wrapit=True, dry_run=False, debug=False )
         self.assertEqual(out, 0, 'run failed')
 
 if __name__ == '__main__':
