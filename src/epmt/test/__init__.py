532d1ecdb25d1583802ca6c2e4bfef8897f26354
Ian Laflotte <ilaflott@gmail.com>
2025-03-27 14:22:58 -0400

    prepping for a pip-install-from-hosted-zip-file test

--- src/epmt/test/__init__.py
+++ src/epmt/test/__init__.py
@@ -7,17 +7,13 @@ from os import environ
 from datetime import datetime
 import pandas as pd
 
-# append the parent directory of this test to the module search path
-#from os.path import dirname
-#sys.path.append(dirname(__file__) + "/..")
-
 from epmt.epmtlib import epmt_logging_init, capture
 ## ERROR
 #epmt_logging_init(-1)
-## WARNING
-#epmt_logging_init(0)
-# INFO
-epmt_logging_init(1)
+# WARNING
+epmt_logging_init(0)
+## INFO
+#epmt_logging_init(1)
 ## DEBUG
 #epmt_logging_init(2)
 
@@ -27,6 +23,6 @@ from epmt.epmt_cmds import epmt_submit
 from epmt.epmtlib import *
 from epmt.orm import *
 
-# this will be used repeatedly in the tests, so  let's store it 
+# this will be used repeatedly in the tests, so let's store it 
 # in a variable instead of repeatedly calling the function
 install_root = get_install_root()
